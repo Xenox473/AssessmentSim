e4ff13da2bcd90efd0642e4f0549125e687298f0
Abhishek Dhar <abhishek473@gmail.com>
2024-08-27 15:16:47 -0400

    Created Questioner Component

---
+++ frontend/src/components/Questioner.jsx
@@ -0,0 +1,26 @@
+import { Stack } from "@mui/material";
+import AnswerButton from "./AnswerButton";
+
+const Questioner = ({
+    question,
+    answers,
+    handleClick
+}) => {
+    return (
+      <div>
+        <h3>{question.title}</h3>
+        <Stack spacing={2}>
+        {answers.map((answer, index) => (
+          <AnswerButton 
+            key={index}
+            answer={answer} 
+            questionId={question.question_id}
+            handleClick={handleClick}
+          />
+        ))}
+        </Stack>
+      </div>
+    );
+}
+
+export default Questioner;
