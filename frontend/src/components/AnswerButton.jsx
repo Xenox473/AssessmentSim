c09c90d3b5887d37a82eedb8c4cd5caea5e2d31d
Abhishek Dhar <abhishek473@gmail.com>
2024-08-28 10:32:26 -0400

    Account for smaller screends

--- frontend/src/components/AnswerButton.jsx
+++ frontend/src/components/AnswerButton.jsx
@@ -1,17 +1,13 @@
 import { Button } from "@mui/material";
 
-const AnswerButton = ({ 
-  answer,
-  questionId,
-  handleClick
-}) => {
+const AnswerButton = ({ answer, questionId, handleClick }) => {
   const style = {
-    width: '50%'    
+    width: '40%',
+    minWidth: '250px',
   };
 
-  
   return (
-    <Button variant="contained" style={style} color="primary" onClick={() => handleClick(questionId, answer.value)}>
+    <Button variant="contained" sx={style} color="primary" onClick={() => handleClick(questionId, answer.value)}>
       {answer.title}
     </Button>
   );
--- frontend/src/components/boxStyles.js
+++ frontend/src/components/boxStyles.js
@@ -2,5 +2,6 @@ export const boxStyles = {
   padding: 2,
   backgroundColor: '#FFFFFF',
   borderRadius: 1,
-  width: '40%'
+  width: '40%',
+  minWidth: '300px'
 }
