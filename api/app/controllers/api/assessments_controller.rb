0cd18692e4ac3986849d28e93a249b5137710226
Abhishek Dhar <abhishek473@gmail.com>
2024-08-28 15:22:41 -0400

    Move logic to service

--- api/app/controllers/api/assessments_controller.rb
+++ api/app/controllers/api/assessments_controller.rb
@@ -6,26 +6,6 @@ class Api::AssessmentsController < ApplicationController
   end
 
   def data
-    answers = Answer.all.sort_by(&:value).pluck(:title, :value).map { |title, value| {title: title, value: value} }
-    questions = Question.all.pluck(:question_id, :title).map { |question_id, title| {question_id: question_id, title: title} }
-    details = {
-      id: "abcd-123",
-      name: "BPDS",
-      disorder: "Cross-Cutting",
-      content: {
-        sections: [
-          {
-            type: "standard",
-            title: "During the past TWO (2) WEEKS, how much (or how often) have you been bothered by the following problems?",
-            answers: answers,
-            questions: questions
-          }
-        ],
-        display_name: "BDS"
-      },
-      full_name: "Blueprint Diagnostic Screener"
-    }
-
-    render json: details
+    render json: AssessmentsService.fetch_data
   end
 end
